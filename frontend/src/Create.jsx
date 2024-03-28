121b46e51a08c23cab8c7ab510639a9d5eed7a5a
CheesyChilly <97239118+CheesyChilly@users.noreply.github.com>
2024-03-28 19:48:23 +0530

    added tailwind

--- frontend/src/Create.jsx
+++ frontend/src/Create.jsx
@@ -1,10 +1,17 @@
-import React from "react";
-
 function Create() {
   return (
-    <div>
-      <input type="text" name="" id="" />
-      <button type="button">Add</button>
+    <div className="p-4">
+      <input
+        type="text"
+        name=""
+        id=""
+        className="w-[300px] p-[9px] ml-2 rounded-[10px] border-2"
+      />
+      <button
+        type="button"
+        className="p-[10px] m-2 bg-[black] rounded-[10px] text-[white] cursor-pointer">
+        Add
+      </button>
     </div>
   );
 }
--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,17 +1,22 @@
 import Create from "./Create";
+import { useState } from "react";
 
 function Home() {
   const [todos, setTodos] = useState([]);
   return (
-    <div>
+    <div className="flex flex-col items-center">
       <h2>Todo list</h2>
       <Create />
-      {todos.length == 0 ? (
+      {todos.length === 0 ? (
         <div>
           <h2>No records</h2>
         </div>
       ) : (
-        todos.map((todo) => <div key="">{todo}</div>)
+        todos.map((todo) => (
+          <div key="">
+            {todo}
+          </div>
+        ))
       )}
     </div>
   );
