5df552b8112b6430dbfd534c0fe4e8a2491ae38a
CheesyChilly <97239118+CheesyChilly@users.noreply.github.com>
2024-03-29 13:17:50 +0530

    Update Home.jsx

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -62,13 +62,21 @@ function Home() {
                   onClick={() => handleDone(todo._id)}
                 />
               )}
-              <p className={todo.done ? "ml-[10px] line-through" : "ml-[10px]"}>
+              <p
+                className={
+                  todo.done
+                    ? "ml-[10px] line-through  text-[#b6b6b6]"
+                    : "ml-[10px]"
+                }>
                 {todo.task}
               </p>
             </div>
             <div className="flex">
               <span className="mr-[5px] ml-[4px]">
-                <BsFillTrashFill className="mr-[5px] font-[15px] cursor-pointer" onClick={() => taskDelete(todo._id)}/>
+                <BsFillTrashFill
+                  className="mr-[5px] font-[15px] cursor-pointer"
+                  onClick={() => taskDelete(todo._id)}
+                />
               </span>
             </div>
           </div>
